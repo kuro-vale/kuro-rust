876f90510c266c039d9c5ebddd2498923d4f7771
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-10-26 21:23:41 -0500

    Update shark_pontoon.rs

--- codewars/8 kyu/shark_pontoon.rs
+++ codewars/8 kyu/shark_pontoon.rs
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/57e921d8b36340f1fd000059
+
 fn shark(pontoon_distance: f64, shark_distance: f64,
          you_speed: f64, mut shark_speed: f64, dolphin: bool) -> String {
     if dolphin {
